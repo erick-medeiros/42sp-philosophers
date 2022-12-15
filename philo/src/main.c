a6be4d4d1fdc650db680b44b9652bb1590950328
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-12-15 16:48:00 -0300

    fix: remove casting

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 10:41:45 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/12/15 16:10:44 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/12/15 16:47:43 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(int argc, char *argv[])
 	t_data	data;
 	int		i;
 
-	if (!check_arguments(argc, (char **)argv))
+	if (!check_arguments(argc, argv))
 		return (EXIT_FAILURE);
-	if (!init_data(&data, argc, (char **)argv))
+	if (!init_data(&data, argc, argv))
 		return (EXIT_FAILURE);
 	i = -1;
 	while (++i < data.num_of_philos)
